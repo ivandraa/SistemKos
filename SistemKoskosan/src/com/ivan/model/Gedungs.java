4bb34372c4262a4617b1852e996ef21a43f74237
Ivandra Oktovan Sembiring <ivandraoktovan@gmail.com>
2018-03-08 16:42:07 +0700

    add new variables

--- SistemKoskosan/src/com/ivan/model/Gedungs.java
+++ SistemKoskosan/src/com/ivan/model/Gedungs.java
@@ -1,5 +1,25 @@
 package com.ivan.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name= "gedungs")
 public class Gedungs {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name="id_gedung")
+	private int id_gedung;
+	@Column(name="nama_gedung")
+	private String nama_gedung;
+	@Column(name="alamat_gedung")
+	private String alamat_gedung;
+	@Column(name="id_gedung_detail")
+	private int id_gedung_detail;
 
 }
